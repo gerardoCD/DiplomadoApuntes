d9b640c313e04a7d9f1eaadf1ca03ebb6a99a7db
Gerardo <ferrari_gerardo@live.com.mx>
2018-09-21 16:45:52 -0500

    14/09/18

---
+++ Clases/14:Septiembre:2018/platicFishes/platicFishesTests/platicFishesTests.swift
@@ -0,0 +1,28 @@
+//
+//  platicFishesTests.swift
+//  platicFishesTests
+//
+//  Created by Gerardo Castillo on 14/09/18.
+//  Copyright © 2018 GerardoCastillo. All rights reserved.
+//
+
+import XCTest
+@testable import platicFishes
+
+class platicFishesTests: XCTestCase {
+    
+    
+    func testExample() {
+        XCTAssert(true)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
